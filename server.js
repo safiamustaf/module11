dbabe907b7af77677292a0652db8a6d7570f5c33
FIRST_NAME LAST_NAME <safiamustaf>
2023-05-23 19:15:14 -0500

    server.js

--- server.js
+++ server.js
@@ -16,11 +16,6 @@ app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-// GET Route for feedback page
-app.get('/feedback', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
-);
-
 // Wildcard route to direct users to a 404 page
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/pages/404.html'))
